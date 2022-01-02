4f37f36c6edc1b34b44940aa5357a5acd0a48de2
Riley-Kilgore <rileyk9@icloud.com>
2022-01-02 09:14:09 -0700

    Updates

---
+++ b2e984c5-2c013a9f1cb6b7f42d0a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkyoctodao_interface=self.webpackChunkyoctodao_interface||[]).push([[702],{474:function(n,e,t){t.d(e,{Waq:function(){return c},fOx:function(){return o}});var c="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z",o="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"}}]);
+//# sourceMappingURL=b2e984c5-2c013a9f1cb6b7f42d0a.js.map
